37f1113eb48f7655f76bdeb02fab8d63dd87c752
itc-s24022 <s24022@std.it-college.ac.jp>
2025-05-30 11:40:00 +0900

    kadai

---
+++ D50Q18kadai.java
@@ -0,0 +1,9 @@
+public class D50Q18kadai {
+    public static void main(String[] args) {
+        int num = 0;
+        while (++num < 11) {
+            System.out.println(num);
+            
+        }
+    }
+}
