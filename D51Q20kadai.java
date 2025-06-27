9867daff29d99565768ae21bf6bbcaf5be086c62
itc-s24022 <s24022@std.it-college.ac.jp>
2025-06-27 11:32:31 +0900

    kadai

---
+++ D51Q20kadai.java
@@ -0,0 +1,7 @@
+public class D51Q20kadai {
+    public static void main(String[] args) {
+        int score = 85;
+        String grade = score >= 80 ? "A" : "B";
+        System.out.println(grade);
+    }
+}
