84d510ecb5952e9db83cc3ab56b2b80064d132f8
itc-s24022 <s24022@std.it-college.ac.jp>
2025-06-27 12:36:44 +0900

    kadai

---
+++ D51Q23kadai.java
@@ -0,0 +1,14 @@
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String a = "春。";
+        String b = "夏。";
+        String c = "秋。";
+        String d = "冬。";
+
+        String[] lists = {a, b, c, d};
+
+        for (String list : lists) {
+            System.out.println(list);
+        }
+    }
+}
