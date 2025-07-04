cc45ecd7c17b7f60013bb7681d6b93bfc86c3bf0
itc-s24022 <s24022@std.it-college.ac.jp>
2025-07-04 11:35:05 +0900

    kadai

---
+++ D51Q27Kadai.java
@@ -0,0 +1,9 @@
+public class D51Q27Kadai {
+    public static final String RED = "赤";
+    public static final String YELLOW = "黄";
+    public static final String BLUE = "青";
+
+    public static void main(String[] args) {
+        System.out.println(BLUE);
+    }
+}
